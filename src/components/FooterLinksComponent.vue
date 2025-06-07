5040433bb6d0f48dcfe5d41db666c9a5e80a8fb2
ERIC GANSA DIAMBOTE <ericgansa01@gmail.com>
2025-06-07 21:38:44 +0200

    fix: marge des liens des footers

--- src/components/FooterLinksComponent.vue
+++ src/components/FooterLinksComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="col-sm-6 col-md-3 col-lg-2 col-xl-2">
-    <div class="footer__links">
+    <div class="footer__links footer__links--alt">
       <h5>{{ title }}</h5>
       <ul>
         <li v-for="(link, index) in links" :key="index" :class="{ 'neutral-bottom': index === links.length - 1 }">
