0ebc20f95260a8859d6d281772d5f0b7167cc767
ERIC GANSA DIAMBOTE <ericgansa01@gmail.com>
2025-08-06 08:31:02 +0100

    fix: formatage du code

--- src/views/Annonces.vue
+++ src/views/Annonces.vue
@@ -3,11 +3,7 @@
         <HeaderComponent />
         <BannerComponent :title="title" :backgroundImage="backgroundImage" :bannerThumb="bannerThumb"
             :breadcrumbs="breadcrumbs" />
-        <!-- Composant de filtre -->
-        <!-- <FilterComponent @update:filters="onFiltersUpdate" /> -->
-        <!-- Composant de résultats, avec les filtres en props -->
         <Proposition2Component :filters="filters" />
-
         <FooterComponent />
     </div>
 </template>
@@ -21,9 +17,6 @@ import HeaderComponent from '@/components/HeaderComponent.vue'
 import BannerComponent from '@/components/BannerComponent.vue'
 import Proposition2Component from '@/components/Proposition2Component.vue';
 import FooterComponent from '@/components/FooterComponent.vue';
-import FilterComponent from '@/components/FilterComponent.vue';
-
-
 
 const backgroundImage = new URL('@/assets/images/banner/banner-bg.png', import.meta.url).href
 // const bannerThumb = new URL('@/assets/images/banner/key-illustration.png', import.meta.url).href
@@ -36,11 +29,5 @@ const filters = ref({
   location: '',
   propertyType: ''
 })
-
-// Méthode appelée quand le formulaire de filtre est soumis
-const onFiltersUpdate = (newFilters) => {
-  filters.value = { ...newFilters }
-}
-
 
 </script>
