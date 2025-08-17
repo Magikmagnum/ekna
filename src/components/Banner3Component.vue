b0cb963c3e33675eed4cae4bed4d1b2462ca56b8
ERIC GANSA DIAMBOTE <ericgansa01@gmail.com>
2025-08-17 22:36:34 +0100

    feat:Vous louez. EKNA s'occupe du reste.

---
+++ src/components/Banner3Component.vue
@@ -0,0 +1,125 @@
+<template>
+    <section class="market market__two section__space over__hi">
+        <div class="container">
+            <div class="market__area">
+                <div class="row d-flex align-items-center">
+                    <div class="col-lg-6 col-xl-5 d-none d-lg-block">
+
+                        <div class="market__thumb thumb__rtl column__space">
+                            <img :src="imageSrc" :alt="imageAlt" />
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-xl-6 offset-xl-1">
+                        <div class="content">
+                            <h5 class="neutral-top" v-html="subtitle"></h5>
+                            <h2>{{ title }}</h2>
+                            <p v-html="description"></p>
+
+                            <div class="btn-group">
+                                <router-link :to="ctaLink" class="button button--effect">
+                                    {{ ctaText }}
+                                </router-link>
+                                <div>
+                                    <img src="@/assets/images/ekna/arrow.svg" alt="Play Store" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script setup>
+
+import { useI18n } from 'vue-i18n'
+const { t } = useI18n()
+
+
+import Store2Component from '@/components/Store2Component.vue';
+defineProps({
+    title: {
+        type: String,
+        default: 'Optimisez la gestion de vos colocataires',
+    },
+    subtitle: {
+        type: String,
+        default: "N'attendez plus",
+    },
+    description: {
+        type: String,
+        default: "<ul><li>• Gérez votre colocation sans stress grâce à EKNA.</li><li>• Déposez votre annonce, recevez des candidats qualifiés et sélectionnez en toute simplicité grâce à notre scoring de dossier et notre algorithme de compatibilité.</li></ul>",
+    },
+    ctaText: {
+        type: String,
+        default: 'Commencer maintenant',
+    },
+    ctaLink: {
+        type: String,
+        default: '/Home',
+    },
+    imageSrc: {
+        type: String,
+        default: new URL('@/assets/images/affiliate-illustration.png', import.meta.url).href,
+    },
+    imageAlt: {
+        type: String,
+        default: 'Affiliate Program',
+    },
+});
+</script>
+
+<style scoped>
+/* p,
+th,
+td,
+li,
+input,
+textarea,
+select,
+label,
+blockquote,
+span {
+    margin-bottom: 36px;
+} */
+
+/* .section__space {
+    padding: 0px;
+} */
+
+/* @media (min-width: 1200px) {
+    .offset-xl-1 {
+        margin-left: 3.333333%;
+    }
+}
+
+@media (min-width: 1200px) {
+    .col-xl-6 {
+        flex: 0 0 auto;
+        width: 55%;
+    }
+} */
+
+/* .market__thumb img {
+    max-width: 24vw;
+} */
+
+
+.btn-group {
+    display: flex;
+    justify-content: start;
+    gap: 37px;
+    align-items: center;
+    margin: 32px 0;
+}
+
+.button {
+    margin-top: 0 !important;
+}
+
+.market .content img {
+    position: unset;
+    margin-left: 0px;
+}
+</style>
--- src/views/Affiliate.vue
+++ src/views/Affiliate.vue
@@ -5,6 +5,7 @@
     <BannerSecondComponent />
     <StepTwoSectionComponent />
     <TestimonialsComponent />
+    <Banner3Component title="Vous louez. Ekna s'occupe du reste." subtitle="Optimisez votre rentabilité et sécurisez vos locations en quelques clics." :imageSrc="imageSrc"/>
     <FooterComponent />
   </div>
 </template>
@@ -15,21 +16,19 @@
 import HeaderComponent from '@/components/HeaderComponent.vue'
 import FooterComponent from '@/components/FooterComponent.vue'
 import BannerComponent from '@/components/BannerComponent.vue'
-import BannerSecondComponent from '@/components/BannerSecondComponent.vue'
+import Banner3Component from '@/components/Banner3Component.vue'
 import StepTwoSectionComponent from '@/components/StepTwoSectionComponent.vue'
 import TestimonialsComponent from '@/components/TestimonialsComponent.vue'
 
+// import BannerSecondComponent from '@/components/BannerSecondComponent.vue'
 
 const backgroundImage = new URL('@/assets/images/banner/program-bg.png', import.meta.url).href
 const bannerThumb = new URL('@/assets/images/banner/affi-thumb.png', import.meta.url).href
 
 const title = "<div>Rejoignez</div> <div>l'espace</div> <div>bailleurs EKNA</div>"
 
-const breadcrumbs = [
-  { label: 'Home', path: '/' },
-  { label: 'Pages', path: '/coucou' },
-  { label: 'Affiliate Program', path: '/coucou/papa' }
-]
+
+const imageSrc = new URL('@/assets/images/market-illustration.png', import.meta.url).href
 </script>
 
 <style>
